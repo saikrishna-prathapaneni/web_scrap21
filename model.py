477680d4e1d0879d3075cc819f0e099df57c794a
saikrishna-prathapaneni <57140923+saikrishna-prathapaneni@users.noreply.github.com>
2021-04-30 02:05:26 +0530

    final

--- model.py
+++ model.py
@@ -1,26 +1,30 @@
 from selenium import webdriver
-from bs4 import BeautifulSoup
 import pandas as pd
 import time
 import datetime
+#import all the necessary libraries
 
 
+#url for the weather data 
 url="https://weather.com/en-IN/weather/tenday/l/Khammam+Telangana?canonicalCityId=d9c48593696ea68859c748a8acf914dc031c166665e258c1d8afa0a685b8ee65"
-driver = webdriver.Chrome('c:/chromedriver')
+#use of webdriver to activate communicate with the site
+driver = webdriver.Chrome('c:/chromedriver')# change the location of the driver with repective the system used 
 driver.get(url)
 
+#variables for the data
 location=[]
 temperature=[]
 precipitation=[]
 humid=[]
-
 timeframe=[]
-i=0
+
+#main program implements the collection of data every four hours untill keyboard interrupts
 while True:
      
           try:
-                time.sleep(2)
+                time.sleep(14400)#change the time here for faster data collection 
                 driver.refresh()
+                #div elements on the website to collect the data
                 loc=driver.find_element_by_class_name("LocationPageTitle--PresentationName--Injxu")
                 temp=driver.find_element_by_class_name("DailyContent--temp--_8DL5")
                 prec=driver.find_element_by_class_name("DailyContent--value--3Xvjn")
@@ -30,13 +34,14 @@ while True:
                 temperature.append(temp.text)
                 humid.append(humidity.text)
                 precipitation.append(prec.text)
-                i=i+1
-          except KeyboardInterrupt:
-             
-             print("data collected")
+                
+          except KeyboardInterrupt: 
+             print("data collected") #data collection untill keyboard interrupts
              break
-        # If you actually want the program to exit
-        
+                                   # If you actually want the program to exit
+
+
+#pandas is used to convert the data into csv file  
 df = pd.DataFrame({'location':location,'timeframe':timeframe,'temperature':temperature,'humid':humid,'precipitate':precipitation}) 
 df.to_csv('weatherdata.csv', index=False, encoding='utf-8')
 
