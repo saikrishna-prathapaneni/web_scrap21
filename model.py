a90caa40614f48c76f326e81df39887f7c2cbc89
saikrishna-prathapaneni <57140923+saikrishna-prathapaneni@users.noreply.github.com>
2021-04-30 01:45:11 +0530

    finalmod

--- model.py
+++ model.py
@@ -15,31 +15,31 @@ precipitation=[]
 humid=[]
 
 timeframe=[]
-
+i=0
 while True:
-     time.sleep(100)
-     driver.refresh()
-     loc=driver.find_element_by_class_name("LocationPageTitle--PresentationName--Injxu")
-     temp=driver.find_element_by_class_name("DailyContent--temp--_8DL5")
-     prec=driver.find_element_by_class_name("DailyContent--value--3Xvjn")
-     humidity=driver.find_element_by_class_name("DetailsTable--value--1F3Ze")
-     timeframe.append(datetime.now())
-     location.append(loc.text)
-     temperature.append(temp.text)
-     humid.append(humidity.text)
-     precipitation.append(prec.text)
-    
      
-
+          try:
+                time.sleep(2)
+                driver.refresh()
+                loc=driver.find_element_by_class_name("LocationPageTitle--PresentationName--Injxu")
+                temp=driver.find_element_by_class_name("DailyContent--temp--_8DL5")
+                prec=driver.find_element_by_class_name("DailyContent--value--3Xvjn")
+                humidity=driver.find_element_by_class_name("DetailsTable--value--1F3Ze")
+                timeframe.append(datetime.datetime.now())
+                location.append(loc.text)
+                temperature.append(temp.text)
+                humid.append(humidity.text)
+                precipitation.append(prec.text)
+                i=i+1
+          except KeyboardInterrupt:
+             
+             print("data collected")
+             break
+        # If you actually want the program to exit
+        
 df = pd.DataFrame({'location':location,'timeframe':timeframe,'temperature':temperature,'humid':humid,'precipitate':precipitation}) 
 df.to_csv('weatherdata.csv', index=False, encoding='utf-8')
-#for a in soup.findAll('span',href=True, attrs={'class':'DailyContent'}):
-#    name=a.find('div', attrs={'class':'_8DL5'})
 
-
-
-with open('scraped.txt','w') as file:
-     file.write(soup)
 
 
 
